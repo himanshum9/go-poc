6de327b89fc0843fd9abae3f4c84d5d3a168e3ed
himanshum9 <contact.himanshum9@gmail.com>
2025-03-25 11:08:58 +0530

    resolved Yoda issue

--- go-unit-tests/calculator/calculator_test.go
+++ go-unit-tests/calculator/calculator_test.go
@@ -6,7 +6,16 @@ func TestDiscountApplied(t *testing.T) {
 	calculator := NewDiscountCalculator(100, 10)
 	amount := calculator.CalculateDiscount(110)
 
-	if 100 != amount {
+	if amount != 100 {
+		t.Fail()
+	}
+}
+
+func TestDiscountNotApplied(t *testing.T) {
+	calculator := NewDiscountCalculator(100, 10)
+	amount := calculator.CalculateDiscount(50)
+
+	if amount != 50 {
 		t.Fail()
 	}
 }
