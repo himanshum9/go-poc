da4af1e67eabfec71a6e1c218732f649e585f6f8
himanshum9 <contact.himanshum9@gmail.com>
2025-06-10 11:16:48 +0530

    Costum error handling

---
+++ customErrorHandling/main.go
@@ -0,0 +1,22 @@
+package main
+
+import "fmt"
+
+type MyError struct {
+	Msg string
+}
+
+func (m MyError) Error() string {
+	return m.Msg
+}
+
+func doSomething() error {
+	return MyError{Msg: "Something went wrong"}
+}
+
+func main() {
+	err := doSomething()
+	if err != nil {
+		fmt.Println(err)
+	}
+}
